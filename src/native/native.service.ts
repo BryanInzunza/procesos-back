9d0dbfe62c68610a8b211288d5755fa54bf23a3a
Bryan Inzunza <l20170704@culiacan.tecnm.mx>
2025-02-04 08:21:22 -0600

    Limitar la creación de hilos

--- src/native/native.service.ts
+++ src/native/native.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
-const addon = require('../../build/Release/addon');
+const addon = require('../../build/Release/addon.node');
 
 @Injectable()
 export class NativeService {
+  private threadCount = 0;
+  private readonly maxThreads = 1;
+  private isBlocked = false;
+
   startLoops(): string {
-    return addon.startLoops();
+    if (this.isBlocked) {
+      return 'Procesos ya iniciados';
+    }
+
+    if (this.threadCount >= this.maxThreads) {
+      this.isBlocked = true;
+    }
+
+    this.threadCount++;
+    addon.startLoops();
+    return `Thread started`;
   }
 
   getIteration(loopNumber: number): { iteration: number, threadId: string, pid: number } {
-    const result = addon.getIteration(loopNumber);
-    return {
-      iteration: result.iteration,
-      threadId: result.threadId,
-      pid: result.pid,
-    };
+    return addon.getIteration(loopNumber);
   }
 
   getPIDs(): number[] {
